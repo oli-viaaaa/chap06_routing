aa3300e4beecc0785f1393fffd05bc1e7684c501
oli-viaaaa <4688ehowl@naver.com>
2023-06-23 17:15:54 +0900

    23.06.23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+import About from './component/About';
+import Contact from './component/Contact';
+import Home from './component/Home';
+import Mypage from './component/Mypage';
+import NotFound from './component/NotFound';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        
+        <nav>
+          <ul>
+            <li>
+              <Link to ="/">Home</Link>
+            </li>
+            <li>
+              <Link to ="/about">About</Link>
+            </li>
+            <li>
+              <Link to ="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to ="/mypage">Mypage</Link>
+            </li>
+            <li>
+              <Link to ="/없는주소">NotFound</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+
